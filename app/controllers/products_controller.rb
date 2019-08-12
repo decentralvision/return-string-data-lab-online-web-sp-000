979cc897fe684a205492cbaf6906e32950284b66
Tom <36858744+decentralvision@users.noreply.github.com>
2019-08-11 20:06:48 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,22 @@
 class ProductsController < ApplicationController
-
+    def create
+        product_hash = params["product"]
+        @product = Product.create(product_params)
+        redirect_to products_path
+    end
+    def index
+        @products = Product.all
+    end
+    def inventory
+        @product = Product.find(params["id"])
+        if @product.inventory.to_i > 0
+            render plain: "true"
+        else
+            render plain: "false"
+        end
+    end
+    private
+    def product_params
+      params.require(:product).permit(:description, :inventory, :name, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+  get 'products/:id/inventory', to: 'products#inventory'
 end
