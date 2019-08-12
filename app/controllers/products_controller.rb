9b20a4a2d9002f7fe822b712e2509cf377b9522c
Tom <36858744+decentralvision@users.noreply.github.com>
2019-08-11 20:44:22 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,19 @@
 class ProductsController < ApplicationController
+    
+    def new
+        @product = Product.new
+    end
+
     def create
         product_hash = params["product"]
         @product = Product.create(product_params)
         redirect_to products_path
     end
+
     def index
         @products = Product.all
     end
+
     def inventory
         @product = Product.find(params["id"])
         if @product.inventory.to_i > 0
@@ -15,12 +22,16 @@ class ProductsController < ApplicationController
             render plain: "false"
         end
     end
+
     def description
         @product = Product.find(params["id"])
         render plain: @product.description
     end
+
     private
+
     def product_params
       params.require(:product).permit(:description, :inventory, :name, :price)
     end
+
 end
